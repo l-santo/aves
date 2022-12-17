55f7dc684ebdbd6e39efac02d0c063e7ec387a96
Leonardo Santo <leonard_sc@hotmail.com>
2022-12-17 06:02:19 -0500

    init commit

---
+++ src/test/java/ec/edu/itsqmet/aves/AvesApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.aves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
